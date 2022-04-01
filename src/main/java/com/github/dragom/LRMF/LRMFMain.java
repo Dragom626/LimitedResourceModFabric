9fc7e128ed11a775fdcd748a6bc50c60578876c2
Dragom626 <unconfigured@null.spigotmc.org>
2022-04-01 03:24:22 -0400

    Base Changes

---
+++ src/main/java/com/github/dragom/LRMF/LRMFMain.java
@@ -0,0 +1,15 @@
+package com.github.dragom.LRMF;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class LRMFMain implements ModInitializer {
+	public static final Logger LOGGER = LoggerFactory.getLogger("modid");
+
+	@Override
+	public void onInitialize() {
+
+		LOGGER.info("Hello Fabric world!");
+	}
+}
